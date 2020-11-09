8511f0facfd19cd27d1a7bfa51b6b3e6417915d8
justiny7 <justinytwu@gmail.com>
2020-11-09 08:41:13 -0800

    484B

--- codeforces/gitgud/484B/main.cpp
+++ codeforces/gitgud/484B/main.cpp
@@ -1,14 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int mxN=1e5+1;
-int n, a[mxN];
+const int mxV=2e6+1;
+int n, ans, a[mxV], lb[mxV];
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> n;
-    for (int i=0; i<n; ++i)
-        cin >> a[i];
-    sort(a, a+n);
+    while (n--) {
+        int x; cin >> x;
+        a[x]=1;
+    }
+    for (int i=2; i<mxV; ++i)
+        lb[i]=(a[i-1]?i-1:lb[i-1]);
+    for (int i=1; i<mxV; ++i)
+        if (a[i])
+            for (int j=i*2; j<mxV; j+=i)
+                ans=max(ans, lb[j]%i);
+    cout << ans << '\n';
 }
 
