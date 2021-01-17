8ce9df29393f3af19ba3b20463434b72e7320f1c
justiny7 <justinytwu@gmail.com>
2021-01-17 12:45:31 -0800

    Created cleaner version of supertrees and wrote my own little solution thing from what I observed

--- ioi/2020/supertrees/README.md
+++ ioi/2020/supertrees/README.md
@@ -1,2 +1,18 @@
 # My first IOI problem attempt!  
-Accidentally solved subtask 5 when doing subtask 4  
+Accidentally solved subtask 5 when doing subtask 4 xd  
+
+__Subtask 1__  
+This is just simply creating a tree - connect all nodes to a single "root" node, create a straight line, etc.  
+
+__Subtask 2__
+Now, we have a collection of connected components. Each connected component has exactly one path from each node to every other node, so each connected component is a tree. Make sure to check whether merging nodes results in nodes that shouldn't have any paths between them to end up in the same component.  
+
+__Subtask 3__
+Here, we have another colleciton of connected components in which every node within a component has exactly two paths to each other node. In other words, each connected component is a cycle. Again, check to make sure merging nodes doesn't result in paths being created between nodes that shouldn't share any paths, and also if there are exactly two nodes in a component, then creating a cycle is impossible.  
+
+__Subtasks 4 & 5__
+The key insight here is to realize that for each connected component, all nodes with exactly one path to another node must exist together in a tree, and all nodes with exactly two paths to another node must be a part of a cycle, leading a structure of either a single tree or multiple trees strung together into a cycle (subtask 4). To check if such a structure can exist, another observation is that every node in a tree must have the exact same number of paths to all other nodes. Within a tree, they all share 1 path, and outside the tree, there are exactly two paths to all other nodes through the cycle (subtask 5). Again, don't forget to check if nodes that shouldn't share any paths are in the same component, creating a cycle is possible, etc.  
+
+__Subtask 6__
+Finally, after a bit of experimenting, it's obvious that there is only one structure that allows exactly three paths from a node to another: something synonymous to a {{0, 1}, {1, 2}, {2, 3}, {3, 0}, {0, 2}} set of edges (a cycle of length four with a diagonal crossing through). It's easy to see that with less than four nodes, it's impossible make three paths between two nodes, and with greater than four nodes, you can only make four or more paths if you split a cycle. Therefore, the final subtask is just checking that for every node with three paths to another node, that the connected component they create follows exactly the structure described above with no other nodes connected to it.  
+
---
+++ ioi/2020/supertrees/subtask-6/clean/main.cpp
@@ -0,0 +1,115 @@
+#include "supertrees.h"
+#include <bits/stdc++.h>
+#define eb emplace_back
+using namespace std;
+using vb=vector<bool>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+
+const int mxN=1e3+1;
+int n;
+vb vis;
+vvi ans;
+
+struct DSU {
+    int par[mxN], sz[mxN];
+    DSU() {
+        for (int i=0; i<n; ++i)
+            par[i]=i, sz[i]=1;
+    }
+    int find(int v) {
+        return (par[v]==v?v:par[v]=find(par[v]));
+    }
+    bool merge(int a, int b) {
+        a=find(a), b=find(b);
+        if (a==b)
+            return 0;
+        if (sz[a]>sz[b])
+            swap(a, b);
+        par[a]=b, sz[b]+=sz[a];
+        return 1;
+    }
+    bool same(int a, int b) {
+        return (find(a)==find(b));
+    }
+};
+
+int construct(vvi p) {
+    n=p.size();
+    ans=vvi(n, vi(n));
+    DSU d[4];
+    for (int i=0; i<n; ++i)
+        for (int j=0; j<n; ++j)
+            if (p[i][j] && p[i][j]^2)
+                d[p[i][j]].merge(i, j);
+    for (int i=0; i<n; ++i)
+        for (int j=0; j<n; ++j) {
+            if (p[i][j]==2)
+                d[2].merge(d[1].find(i), d[1].find(j));
+            else if (!p[i][j]) {
+                for (int k=1; k<4; ++k)
+                    if (d[k].same(i, j))
+                        return 0;
+            }
+        }
+    vis=vb(n);
+    for (int i=0; i<n; ++i) {
+        if (vis[d[1].find(i)])
+            continue;
+        vis[d[1].find(i)]=1;
+        for (int j=0; j<n; ++j)
+            if (i^j && d[1].same(i, j)) {
+                if (p[i]!=p[j])
+                    return 0;
+                ans[i][j]=ans[j][i]=1;
+            }
+    }
+    vis=vb(n);
+    for (int i=0; i<n; ++i) {
+        if (vis[d[2].find(i)])
+            continue;
+        vis[d[2].find(i)]=1;
+        vi v;
+        for (int j=0; j<n; ++j)
+            if (d[2].same(i, j) && find(v.begin(), v.end(), j)==v.end())
+                v.eb(j);
+        int sz=v.size();
+        if (sz==1)
+            continue;
+        if (sz==2)
+            return 0;
+        for (int j=0; j<sz; ++j) {
+            if (j)
+                ans[v[j]][v[j-1]]=ans[v[j-1]][v[j]]=1;
+            if (j<sz-1)
+                ans[v[j]][v[j+1]]=ans[v[j+1]][v[j]]=1;
+        }
+        ans[v[0]][v.back()]=ans[v.back()][v[0]]=1;
+    }
+    vis=vb(n);
+    for (int i=0; i<n; ++i) {
+        if (vis[d[3].find(i)])
+            continue;
+        vis[d[3].find(i)]=1;
+        vi v;
+        bool f=1;
+        for (int j=0; j<n; ++j) {
+            if (d[3].same(i, j))
+                v.eb(j);
+            if (i^j)
+                f&=(p[i][j]==3 || !p[i][j]);
+        }
+        int sz=v.size();
+        if (sz==1)
+            continue;
+        if (f || sz^4)
+            return 0;
+        ans[v[0]][v[1]]=ans[v[1]][v[0]]=1;
+        ans[v[1]][v[2]]=ans[v[2]][v[1]]=1;
+        ans[v[2]][v[3]]=ans[v[3]][v[2]]=1;
+        ans[v[0]][v[3]]=ans[v[3]][v[0]]=1;
+        ans[v[0]][v[2]]=ans[v[2]][v[0]]=1;
+    }
+    build(ans);
+    return 1;
+}
