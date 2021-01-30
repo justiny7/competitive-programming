fdc45c9f41f4e8ccc6f72cd55648efdd9d1adc44
justiny7 <justinytwu@gmail.com>
2021-01-29 17:06:07 -0800

    Redid legit version of telephone

--- usaco/contests/official/2020-21/Gold January (PROMOTION)/telephone/legit/main.cpp
+++ usaco/contests/official/2020-21/Gold January (PROMOTION)/telephone/legit/main.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 using pi=pair<int, int>;
 
-const int mxN=5e4+1, mxK=51, INF=0x3f3f3f3f;
+const int mxN=5e4+1, mxK=51;
 int n, k, breed[mxN], dist[mxN];
-vector<int> breed_list[mxK];
-bool ok[mxK][mxK];
+vector<int> cows[mxK], adj[mxN];
 priority_queue<pi, vector<pi>, greater<pi>> pq;
 
 int main() {
@@ -14,49 +13,44 @@ int main() {
     cin >> n >> k;
     for (int i=1; i<=n; ++i) {
         cin >> breed[i];
-        breed_list[breed[i]].push_back(i);
+        cows[breed[i]].push_back(i);
     }
     for (int i=1; i<=k; ++i) {
         string s; cin >> s;
-        for (int j=1; j<=k; ++j)
-            ok[i][j]=bool(s[j-1]-'0');
-    }
-    pq.emplace(0, 1);
-    dist[1]=0;
-    while (pq.size()) {
-        int d, v;
-        tie(d, v)=pq.top(); pq.pop();
-        if (d^dist[v])
-            continue;
-        for (int i=1; i<=k; ++i) {
-            if (!ok[breed[v]][i] || breed_list[i].empty())
+        for (int j=1; j<=k; ++j) {
+            if (s[j-1]=='0')
                 continue;
-            for (int t=0; t<3; ++t) {
-                int u;
-                if (t==0) {
-                    auto it=lower_bound(breed_list[i].begin(), breed_list[i].end(), v);
-                    if (it!=breed_list[i].begin())
-                        --it;
-                    else
-                        continue;
-                    u=*it;
+            if (breed[n]^j) {
+                for (int l:cows[i]) {
+                    auto it=upper_bound(cows[j].begin(), cows[j].end(), l);
+                    if (it!=cows[j].end())
+                        adj[l].push_back(*it);
+                    it=lower_bound(cows[j].begin(), cows[j].end(), l);
+                    if (it!=cows[j].begin())
+                        adj[l].push_back(*prev(it));
                 }
-                else if (t==1) {
-                    auto it=upper_bound(breed_list[i].begin(), breed_list[i].end(), v);
-                    if (it==breed_list[i].end())
-                        continue;
-                    u=*it;
-                }
-                else
-                    u=breed_list[i].back();
-                int nd=d+abs(v-u);
-                if (dist[u]>nd) {
-                    dist[u]=nd;
-                    pq.emplace(nd, u);
-                }
+            }
+            else {
+                for (int l:cows[i])
+                    adj[l].push_back(n);
             }
         }
     }
-    cout << (dist[n]==INF?-1:dist[n]) << '\n';
+    dist[1]=0;
+    pq.emplace(0, 1);
+    while (pq.size()) {
+        int d, v;
+        tie(d, v)=pq.top(); pq.pop();
+        if (dist[v]^d)
+            continue;
+        for (int u:adj[v]) {
+            int nd=d+abs(u-v);
+            if (dist[u]>nd) {
+                dist[u]=nd;
+                pq.emplace(nd, u);
+            }
+        }
+    }
+    cout << (dist[n]==0x3f3f3f3f?-1:dist[n]) << '\n';
 }
 
