2f956bb024d71bacf9023064f547c125d12bf588
justiny7 <justinytwu@gmail.com>
2021-06-02 12:51:07 -0700

    Round Trip II - fixed code that got hacked

--- cses/graph-algorithms/round-trip-ii/main.cpp
+++ cses/graph-algorithms/round-trip-ii/main.cpp
@@ -1,53 +1,41 @@
 #include <bits/stdc++.h>
-#define pb push_back
 using namespace std;
 
-const int mxN=1e5+1;
-int n, m, p[mxN];
-vector<int> adj[mxN], ans;
-bool vis[mxN], vis2[mxN];
+const int N = 1e5 + 69;
+int n, m, par[N], vis[N];
+vector<int> adj[N], ans;
 
-void dfs(int v, int par=-1) {
-    vis[v]=1, p[v]=par;
+void dfs(int v, int p = 0) {
+    vis[v] = 1, par[v] = p;
     for (int u : adj[v]) {
         if (!vis[u])
             dfs(u, v);
-        else if (vis[u] && !vis2[u]){
-            ans.pb(u);
-            while (v!=u) {
-                ans.pb(v);
-                v=p[v];
+        else if (vis[u] == 1) {
+            ans.push_back(u);
+            while (v != u) {
+                ans.push_back(v);
+                v = par[v];
             }
-            if (v<0) {
-                ans.clear();
-                continue;
-            }
-            ans.pb(u);
+            ans.push_back(u);
             cout << ans.size() << '\n';
-            for (int i=ans.size()-1; i>=0; --i) {
-                cout << ans[i];
-                if (i)
-                    cout << " ";
-            }
-            cout << '\n';
+            for (int i = ans.size() - 1; ~i; --i)
+                cout << ans[i] << " \n"[i == 0];
             exit(0);
         }
     }
-    vis2[v]=1;
+    vis[v] = 2;
 }
 
-int main(void) {
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
     cin >> n >> m;
-    for (int i=0; i<m; ++i) {
-        int a, b;
-        cin >> a >> b;
-        adj[a].pb(b);
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
     }
-    for (int i=1; i<=n; ++i)
-        if (!vis[i]) {
-            memset(vis, 0, sizeof(vis));
+    for (int i = 1; i <= n; ++i)
+        if (!vis[i])
             dfs(i);
-        }
     cout << "IMPOSSIBLE\n";
 }
-
--- cses/graph-algorithms/round-trip/main.cpp
+++ cses/graph-algorithms/round-trip/main.cpp
@@ -7,20 +7,20 @@ vector<int> adj[100001], ans;
 bool vis[100001];
 
 void dfs(int cur, int par) {
-    p[cur]=par;
-    vis[cur]=1;
+    p[cur] = par;
+    vis[cur] = 1;
     for (int u : adj[cur]) {
-        if (u==par)
+        if (u == par)
             continue;
         if (!vis[u])
             dfs(u, cur);
         else {
-            while (cur!=u) {
+            while (cur != u) {
                 ans.push_back(cur);
-                cur=p[cur];
+                cur = p[cur];
             }
             ans.push_back(u);
-            cout << ans.size()+1 << '\n';
+            cout << ans.size() + 1 << '\n';
             cout << u;
             for (int a : ans)
                 cout << " " << a;
@@ -32,13 +32,13 @@ void dfs(int cur, int par) {
 
 int main(void) {
     cin >> n >> m;
-    for (int i=0; i<m; ++i) {
+    for (int i = 0; i < m; ++i) {
         int a, b;
         cin >> a >> b;
         adj[a].pb(b);
         adj[b].pb(a);
     }
-    for (int i=1; i<=n; ++i)
+    for (int i = 1; i <= n; ++i)
         if (!vis[i])
             dfs(i, -1);
     cout << "IMPOSSIBLE\n";
